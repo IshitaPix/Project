be38d8a203695de515bdd7247f56ef27e74e374d
IshitaPix <149964438+IshitaPix@users.noreply.github.com>
2024-04-12 22:05:04 +0530

    Add files via upload

---
+++ Web/script_t.js
@@ -0,0 +1,49 @@
+  
+   function myMenuFunction() {
+    var i = document.getElementById("navMenu");
+
+    if(i.className === "nav-menu") {
+        i.className += " responsive";
+    } else {
+        i.className = "nav-menu";
+    }
+   }
+ 
+
+
+    var a = document.getElementById("loginBtn");
+    var b = document.getElementById("registerBtn");
+    var x = document.getElementById("login");
+    var y = document.getElementById("register");
+
+    function login() {
+        x.style.left = "4px";
+        y.style.right = "-520px";
+        a.className += " white-btn";
+        b.className = "btn";
+        x.style.opacity = 1;
+        y.style.opacity = 0;
+    }
+
+    function register() {
+        x.style.left = "-510px";
+        y.style.right = "5px";
+        a.className = "btn";
+        b.className += " white-btn";
+        x.style.opacity = 0;
+        y.style.opacity = 1;
+    }
+
+
+    /*--------------------------MAIN SITE------------------*/
+
+    var navLinks = document.getElementById("navLinks");
+    /*var headerLinks = document.getElementById("headerLinks");*/
+    
+    function showMenu(){
+        navLinks.style.right = "0";
+    }
+    function hideMenu(){
+        navLinks.style.right = "-200px";
+    }
+
